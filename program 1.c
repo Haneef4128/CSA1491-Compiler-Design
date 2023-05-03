cd8ea02bbff9cb83a9d4ba03e12b4d187a7bc202
Haneef4128 <114045201+Haneef4128@users.noreply.github.com>
2023-05-03 10:26:11 +0530

    Identifying

--- program 1.c
+++ program 1.c
@@ -1,57 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <regex.h>
-
-#define MAX_IDENTIFIER_LENGTH 50
-
-int main() {
-    char input[] = "if (x == 5) {\n"
-                   "    /* This is a comment */\n"
-                   "    y = x + 1;\n"
-                   "    return y;\n"
-                   "}";
-
-    regex_t regex[6];
-    int reti;
-    size_t nmatch = 1;
-    regmatch_t pmatch[1];
-
-    // Compile the regular expressions
-    reti = regcomp(&regex[0], "[a-zA-Z][a-zA-Z0-9]*", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile regular expression\n");
-        exit(1);
-    }
-
-    reti = regcomp(&regex[1], "[0-9]+", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile regular expression\n");
-        exit(1);
-    }
-
-    reti = regcomp(&regex[2], "if|else|while|for|return", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile regular expression\n");
-        exit(1);
-    }
-
-    reti = regcomp(&regex[3], "\\+|-|\\*|/|%|=|==|!=|<|<=|>|>=|&&|\\|\\|", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile regular expression\n");
-        exit(1);
-    }
-
-    reti = regcomp(&regex[4], "\\(|\\)|\\{|\\}|\\[|\\]|;|,", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile regular expression\n");
-        exit(1);
-    }
-
-    reti = regcomp(&regex[5], "/\\*.*?\\*/|", REG_EXTENDED);
-    if (reti) {
-        fprintf(stderr, "Could not compile regular expression\n");
-        exit(1);
-    }
-
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main()
+{
+	int i,ic=0,m,cc=0,oc=0,j;
+	char b[30],operators[30],identifiers[30],constants[30];
+	printf("enter the string : ");
+	scanf("%[^\n]s",&b);
+	for(i=0;i<strlen(b);i++)
+	{
+    	if(isspace(b[i]))
+		{
+    	    continue;
+    	}
+    	else if(isalpha(b[i]))
+		{
+        	identifiers[ic] =b[i];
+        	ic++;
+    	}
+    	else if(isdigit(b[i]))
+		{
+        	m=(b[i]-'0');
+        	i=i+1;
+        	while(isdigit(b[i]))
+			{
+            	m=m*10 + (b[i]-'0');
+            	i++;
+        	}
+        	i=i-1;
+        	constants[cc]=m;
+        	cc++;
+    	}
+    	else
+		{
+        	if(b[i]=='*')
+			{
+            	operators[oc]='*';
+            	oc++;
+        	}
+        	else if(b[i]=='-')
+			{
+            	operators[oc]='-';
+            	oc++;
+        	}
+        	else if(b[i]=='+')
+			{
+            	operators[oc]='+';
+            	oc++;
+        	}
+         	else if(b[i]=='=')
+			{
+            	operators[oc]='=';
+            	oc++;
+        	}
+  		}
+	}
+    printf(" identifiers : ");
+     for(j=0;j<ic;j++)
+	 {
+        printf("%c ",identifiers[j]);
+     }
+    printf("\n constants : ");
+     for(j=0;j<cc;j++)
+	 {
+        printf("%d ",constants[j]);
+     }
+    printf("\n operators : ");
+      for(j=0;j<oc;j++)
+	  {
+        printf("%c ",operators[j]);
+      }
 }
